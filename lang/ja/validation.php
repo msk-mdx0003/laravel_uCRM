37226392b3e98e08a2b1dda70344a1a941db709e
msk-mdx0003 <msk-mdx0003@gmail.com>
2024-05-24 12:12:39 +0900

    商品関連

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -149,8 +149,11 @@ return [
     */
     'attributes' => [
         'title' => '件名',
-        'content' => '本文'
-    ]
+        'content' => '本文',
+        'name' => '名',
+        'memo' => 'メモ',
+        'price' => '商品価格'
+    ] 
 
 ];
 
