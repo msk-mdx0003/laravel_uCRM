8ef334c542834438d109c844c4ce3f1d443d9582
msk-mdx0003 <msk-mdx0003@gmail.com>
2024-05-26 12:04:01 +0900

    顧客検索

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,11 +25,22 @@ Route::get('/inertia-test', function () {
     }
 );
 
+
+
+
+
 Route::resource('items', ItemController::class
     )->middleware(['auth', 'verified']);
 
 Route::resource('customers', CustomerController::class
 )->middleware(['auth', 'verified']);
+
+Route::resource('purchases', PurchaseController::class
+)->middleware(['auth', 'verified']);
+
+
+
+
 
 Route::get('/component-test', function () {
     return Inertia::render('ComponentTest');
